87db825a561ea91f1be93a901775acc678748afa
Reginaldo Alves <101367348+reginaldoa@users.noreply.github.com>
2022-06-04 01:40:48 -0300

    Add files via upload

---
+++ Transformando em JSON.js
@@ -0,0 +1,12 @@
+const pessoas = [
+    {nome: 'João'},
+    {nome: 'Maria'},
+    {nome: 'Paulo'},
+    {nome: 'André'},
+    {nome: 'Vinicius'},
+]
+
+
+
+const json =JSON.stringify(pessoas,'',2)
+console.log(json)
